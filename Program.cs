b983b42042dd6a0f9761945f997c62554258c59c
thaodacat <thaodacat_08@icloud.com>
2020-08-21 21:36:32 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Boxing();
+        }
+
+        static void Boxing()
+        {
+            List<object> theList = new List<object>();
+            // Console.WriteLine(String.Join(", ", theList));
+
+            theList.Add(7);
+            theList.Add(28);
+            theList.Add(-1);
+            theList.Add(true);
+            theList.Add("chair");
+            // Console.WriteLine(String.Join(", ", theList));
+
+            foreach(var i in theList)
+            {
+                Console.WriteLine(i);
+            }
+
+            int sum = 0;
+            foreach(var i in theList)
+            {
+                if(i is int)
+                {
+                    sum += (int)i;
+                }
+
+            }
+            Console.WriteLine($"Sum is {sum}");
+        }
+    }
+}
